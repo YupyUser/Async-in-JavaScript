41c2e84a4d6e24469cb48c73c7134b42d8f9df5a
YupiUser <Miragex69xWindow@gmail.com>
2023-04-13 21:49:42 +0300

    made all needed

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-let codeArea = document.querySelector('#codeArea');
-let answerArea = document.querySelector('.right_answers');
+    setTimeout(0,console.log(1));
 
-function test () {
+
+    console.log(2);
+
+
+    Promise.resolve().then(()=> console.log(3));
+
+
+    new Promise((res) => {
+        console.log(4)
+        res(null);
+    }).then(()=> console.log(5));
+
     
-}
+    console.log(6);
+
+
+    Promise.resolve().then(()=> console.log(7));
+
+    new Promise((res) => {
+        console.log(8)
+        res(null);
+    }).then(()=> console.log(9));
+    
